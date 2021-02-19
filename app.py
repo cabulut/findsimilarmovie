629faf26a62b03aac4cebd1501d311687892b284
Caner Bulut <bulut.caner@outlook.com>
2021-02-20 00:31:59 +0100

    web app

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[5]:
 
 
 import streamlit as st
 import pandas as pd
 
 
-# In[2]:
+# In[6]:
 
 
 final = pd.read_csv("cluster.csv",index_col=0)
